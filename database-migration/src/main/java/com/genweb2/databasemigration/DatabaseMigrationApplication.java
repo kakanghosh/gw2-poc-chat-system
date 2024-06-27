8847aa6bcbe8c24747a7455be6c4af4aa56241f1
kakan <kakanghosh69@gmail.com>
2024-06-27 20:04:08 +0600

    added db migration service

---
+++ database-migration/src/main/java/com/genweb2/databasemigration/DatabaseMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.genweb2.databasemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMigrationApplication.class, args);
+	}
+
+}
---
+++ database-migration/src/test/java/com/genweb2/databasemigration/DatabaseMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genweb2.databasemigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
