ea90f8691927267fb6aa0e5070cd2e2619c6f535
kakan <kakanghosh69@gmail.com>
2024-06-27 19:20:10 +0600

    init project

---
+++ enchanted-message-bridge/src/main/java/com/genweb2/emb/EnchantedMessageBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.genweb2.emb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchantedMessageBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnchantedMessageBridgeApplication.class, args);
+	}
+
+}
---
+++ enchanted-message-bridge/src/test/java/com/genweb2/emb/EnchantedMessageBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genweb2.emb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnchantedMessageBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
