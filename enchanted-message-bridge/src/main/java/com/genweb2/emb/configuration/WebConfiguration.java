1a29299c7828c84c0e3712675dda821713f75ffd
kakan <kakanghosh69@gmail.com>
2024-06-29 11:01:36 +0600

    added cors config

--- enchanted-message-bridge/src/main/java/com/genweb2/emb/configuration/WebConfiguration.java
+++ enchanted-message-bridge/src/main/java/com/genweb2/emb/configuration/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.genweb2.emb.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration {
 
+    @Value("${web.config.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfiguration() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- enchanted-message-bridge/src/main/java/com/genweb2/emb/configuration/WebSocketConfig.java
+++ enchanted-message-bridge/src/main/java/com/genweb2/emb/configuration/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.genweb2.emb.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -14,6 +15,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final SubscriptionInterceptor subscriptionInterceptor;
+    @Value("${web.config.allowed-origins}")
+    private String allowedOrigins;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -23,7 +26,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/message-bridge");
+        registry.addEndpoint("/message-bridge")
+                .setAllowedOrigins(allowedOrigins);
     }
 
     @Override
